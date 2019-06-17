a8ee365de1819c86d8b65632b4c7fa5351f6e0c5
tinatasha <tashambiti@gmail.com>
2019-06-17 22:13:19 +0300

    add jquery

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,24 @@ else if(pizzaCrustValue == Stuffed){
 else{
     return 50
 };
+$('#top1').click(function () {
+    var toppingsValue1 = $(':checked').map(function () {
+        return this.value;
+    }).get();
+    console.log(toppingsValue);
+});
+
+$('#top2').click(function () {
+    var toppingsValue2 = $(':checked').map(function () {
+        return this.value;
+    }).get();
+    console.log(toppingsValue2);
+});
+
+$('#top3').click(function () {
+    var toppingsValue3 = $(':checked').map(function () {
+        return this.value;
+    }).get();
+    console.log(toppingsValue3);
+});
+
