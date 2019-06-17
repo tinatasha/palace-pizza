6960e3bc37338128b024ba220080ee45fd7b0da5
tinatasha <tashambiti@gmail.com>
2019-06-17 23:31:18 +0300

    add checkout

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,14 @@
-var pizzaSize = $("#size").val();
-var pizzaSizeValue = parseInt(pizzaSize);
-if(pizzaSizeValue == Large){
-    return 1000
-}
-else if(pizzaSizeValue == Medium){
-    return 800
-}
-else{
-    return 600
-};
 
-var pizzaCrust = $("#crust").val();
-var pizzaCrustValue = parseInt(pizzaCrust);
-if(pizzaCrustValue == Thick){
-    return 200
-}
-else if(pizzaCrustValue == Stuffed){
-    return 100
-}
-else{
-    return 50
-};
-$('#top1').click(function () {
-    var toppingsValue1 = $(':checked').map(function () {
-        return this.value;
-    }).get();
-    console.log(toppingsValue);
-});
-
-$('#top2').click(function () {
-    var toppingsValue2 = $(':checked').map(function () {
-        return this.value;
-    }).get();
-    console.log(toppingsValue2);
-});
-
-$('#top3').click(function () {
-    var toppingsValue3 = $(':checked').map(function () {
-        return this.value;
-    }).get();
-    console.log(toppingsValue3);
-});
-
-var pizzaNo = $("#pizzaNumber").value();
-var actualPizzaNo = parseInt(pizzaNo);
-
-var pizzaDelivery = $("#delivery").val();
-var actualPizzaDelivery = parseInt(pizzaDelvery);
-if(actualPizzaDelivery == "Yes"){
-    alert("Delivery costs Ksh. 500");
-    prompt("Enter your destination");
-alert("your order will be delivered to your location");
-};
- 
-$("#checkout").click(function(){
-var finalPrize = document.querySelector("#finalPrize");
-
-$("#finalOrder")
+$(document).ready(function(){
+    alert("ggg");
+    $("#yesButton").click(function(){
+        alert("Delivery costs Ksh.500");
+        prompt("Enter your destination");
+    });
+    $("#noButton").click(function(){
+    alert("Remember to try our delivery services next time.")
+    });
+    $("#checkout").click(function(){
+        alert("Your order will be delivered to your location");
+    });
 });
